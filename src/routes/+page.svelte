27471362ad5e8d9971820c21bba1305e43c70908
Damian Soto <soto.platero@gmail.com>
2024-08-08 21:20:44 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+<svelte:head>
+	<title>Fire Extinguisher Prices</title>
+	<meta name="description" content="Comparison of all Fire Extinguisher on Amazon sorted by price." />
+</svelte:head>
+
+<section class="flex flex-col h-screen items-center justify-center">
+    <h1 class="text-4xl">Fire Extinguisher Prices</h1>
+    <p>As an Amazon Associate I earn from qualifying purchases</p>
+    <a href="https://amzn.to/3WGMtja" target="_blank" class="mt-6 font-bold">Buy on Amazon</a>
+</section>
+
