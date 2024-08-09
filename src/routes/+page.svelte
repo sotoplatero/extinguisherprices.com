6ca4dcc1ecf75db6aaf5f5524c4a8619cc56d531
Damian Soto <soto.platero@gmail.com>
2024-08-09 08:28:14 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	<meta name="description" content="Comparison of all Fire Extinguisher on Amazon sorted by price." />
 </svelte:head>
 
-<section class="flex flex-col h-screen items-center justify-center">
+<section class="flex flex-col h-screen items-center justify-center text-center px-8">
     <h1 class="text-4xl">Fire Extinguisher Prices</h1>
     <p>As an Amazon Associate I earn from qualifying purchases</p>
     <a href="https://amzn.to/3WGMtja" target="_blank" class="mt-6 font-bold">Buy on Amazon</a>
