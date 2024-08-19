f2f67ca8a178ae243a2d517a07ac955e80d501d3
Damian Soto <soto.platero@gmail.com>
2024-08-19 13:47:14 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,6 @@
 <section class="flex flex-col h-screen items-center justify-center text-center px-8">
     <h1 class="text-4xl leading-snug">Fire Extinguisher Prices</h1>
     <p>As an Amazon Associate I earn from qualifying purchases</p>
-    <a href="https://amzn.to/3WGMtja" target="_blank" class="mt-6 font-bold">Buy on Amazon</a>
+    <a href="https://amzn.to/3WGMtja" target="_blank" class="mt-6 font-bold p-3 bg-blue-500 text-white rounded">Buy on Amazon</a>
 </section>
 
