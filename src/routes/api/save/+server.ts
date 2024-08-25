3ae885c66971bc29a3b9733d3be6179dec935acc
Damian Soto <soto.platero@gmail.com>
2024-08-25 05:45:33 -0400

    update

--- src/routes/api/save/+server.ts
+++ src/routes/api/save/+server.ts
@@ -4,7 +4,7 @@ import amazonPaapi from 'amazon-paapi';
 import type { RequestHandler } from '../$types';
 import { table } from '$lib/deta';
 
-export async function GET({url}): Promise<Response> {
+export async function POST({url}): Promise<Response> {
 
     // if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     //     return error(401,'Unauthorized', )
