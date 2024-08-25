14e4419c438db6264acc448c807277676f7a992b
Damian Soto <soto.platero@gmail.com>
2024-08-25 14:09:46 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,27 @@
 
     // console.log(data)
     let q =''
+    let minprice = ''
+    let maxprice = ''
     let products = data?.products || []
 
-    $: if (q) {
-        products = data?.products.filter(product => 
-            !/bracket|cable|cabinet|Gauges/i.test(product.ItemInfo?.Title?.DisplayValue) && 
-            product.ItemInfo?.Title?.DisplayValue.includes(q)
-    
-        )
+    function formatPrice(price) {
+        return parseFloat(price?.replace(/\$/,'') || 0)
     }
 
-    // $: console.log(products)
+    $: if (q || minprice || maxprice) {
+        products = data?.products
+            .filter(product => 
+                !/bracket|cable|cabinet|Gauges/i.test(product.ItemInfo?.Title?.DisplayValue) && 
+                product.ItemInfo?.Title?.DisplayValue.includes(q)
+            ).filter(product => 
+                !!minprice ? formatPrice(product.Offers?.Listings[0].Price.DisplayAmount) >= minprice : true 
+            ).filter(product => {
+                return  !!maxprice ? formatPrice(product.Offers?.Listings[0].Price.DisplayAmount) <= maxprice : true 
+            })
+    }
+
+    // $: console.dir(maxprice)
 </script>  
 
 <svelte:head>
@@ -30,18 +40,18 @@
     <div class="flex items-end gap-2">
         <label for="q" class="">
             <div>Search</div>
-            <input type="text" name="q" bind:value={q} class="input input-bordered">
+            <input type="text" name="q" bind:value={q} class="input input-bordered input-sm rounded">
         </label>
         <label for="minprice">
             <div>Min Price</div>
-            <input type="text" name="minprice" class="input input-bordered w-20">
+            <input type="number" name="minprice" bind:value={minprice} class="input input-bordered input-sm rounded w-20">
         </label>
         <label for="minprice">
             <div>Max Price</div>
             
-            <input type="text" name="maxprice" class="input input-bordered w-20">
+            <input type="number" name="maxprice" bind:value={maxprice}  class="input input-bordered input-sm rounded w-20">
         </label>
-        <button class="btn">Search</button>
+        <button class="btn btn-sm rounded">Search</button>
     </div>
     <Products {products} />
 
